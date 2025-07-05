db8a6f8e0fba8bd691ab0a3949f85cc5e4f2de59
Sarieh1 <sariehmohd@gmail.com>
2025-07-05 23:10:00 +0300

    fix

--- src/hooks/course/useGetAllCoursesNoRole.ts
+++ src/hooks/course/useGetAllCoursesNoRole.ts
@@ -7,7 +7,7 @@ const useGetAllCoursesNoRole = () => {
   const {t} = useTypedTranslation()
   const [searchParams, setSearchParams] = useSearchParams()
   const currentPage = searchParams.get("page")
-  const [page, setPage] = useState<number>(Number(currentPage) ?? 1);
+  const [page, setPage] = useState<number>(Math.max(1, Number(currentPage || '1')));
   const {data, isError, error, isFetching, isLoading, isSuccess} = useFetchData({
     queryKey: ["getCoursesNoRole", page], // dynamic queryKey
     queryFn: () => axios.get(`${courseRoute}/all-filter?sortBy=title-atoz&page=${page}`), // pass page param
